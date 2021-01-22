54558f0363f9b2b36560dcee9b943081a6a04546
Saleh Dehghanpour <salehdeh76@gmail.com>
2021-01-22 14:24:16 +0330

    fixed libsvm import issue

--- python/vmaf/svmutil.py
+++ python/vmaf/svmutil.py
@@ -12,12 +12,14 @@ libsvm_path = VmafConfig.root_path('third_party', 'libsvm', 'python')
 
 if libsvm_path not in sys.path:
     # Inject {project}/src/libsvm/python to PYTHONPATH dynamically
-    sys.path.append(libsvm_path)
+    # sys.path.append(libsvm_path)
+    sys.path.insert(0, libsvm_path)
 
 
 try:
     # This import will work only if above injection was meaningful (ie: user has the files in the right place)
     from svmutil import *           # noqa
+    # from third_party.libsvm.python.svmutil import *           # noqa
 
 except ImportError as e:
     print("Can't import svmutil from %s: %s" % (libsvm_path, e))
